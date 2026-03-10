ac70fcc9a7aecd7394d2c994b1d8c2e1c81f1fa3
RTUnreal <unreal@rtinf.net>
2026-03-10 15:27:43 +0100

    asaasas

--- machines/simulator/config.nix
+++ machines/simulator/config.nix
@@ -43,12 +43,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager = {
-    sddm = {
-      enable = true;
-      autoLogin = {
-        relogin = true;
-      };
-    };
+    sddm.enable = true;
     autoLogin.user = "alien";
   };
   services.desktopManager.plasma6.enable = true;
