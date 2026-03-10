1f87e38db4e4f6c150ef80bad4ae09ad63acf174
RTUnreal <unreal@rtinf.net>
2026-03-10 15:37:02 +0100

    disable autoLogin

--- machines/simulator/config.nix
+++ machines/simulator/config.nix
@@ -42,10 +42,7 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager = {
-    sddm.enable = true;
-    autoLogin.user = "alien";
-  };
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
