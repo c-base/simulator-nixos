1a5cfaf3ba68f9e79bc4ee9e2848aa3d8740db29
RTUnreal <unreal@rtinf.net>
2026-03-10 14:55:04 +0100

    fix boot loader

--- machines/simulator/config.nix
+++ machines/simulator/config.nix
@@ -6,8 +6,11 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "simulator"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
