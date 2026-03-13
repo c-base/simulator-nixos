419ff7f26bea57b73f7feab3c254dd684fa2b08c
RTUnreal <unreal@rtinf.net>
2026-03-13 17:07:12 +0100

    configure monado a bit more

--- machines/simulator/config.nix
+++ machines/simulator/config.nix
@@ -82,8 +82,10 @@
     defaultRuntime = true;
   };
   systemd.user.services."monado".environment = {
-    STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
+    XRT_DEBUG_GUI = "1";
+    STEAMVR_LH_ENABLE = "1";
+    LH_STANDBY_ON_EXIT = "1";
   };
   home-manager.users.alien = {
     home.file = {
